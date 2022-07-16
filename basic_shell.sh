3ba96b340f31e2f3c30db443b5dffc3a15b22e46
franckfongang <franckfongang1980@gmail.com>
2022-07-16 01:50:28 -0400

    commit change

--- basic_shell.sh
+++ basic_shell.sh
@@ -9,5 +9,13 @@ echo
 echo what is your name?
 read b
 echo 
+echo What is your profession?
+read c
+echo
+echo What is your favorite show?
+read d 
+echo 
 echo Hello $b
+echo your profession $c is awesome
+$ is a good show 
 echo
