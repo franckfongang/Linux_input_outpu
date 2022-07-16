25b0b2ca847e6145dcb666ec5987976bb53c3dc7
franckfongang <franckfongang1980@gmail.com>
2022-07-16 01:33:59 -0400

    change commit

---
+++ basic_shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author
+#Date
+#Desc
+# read and echo command
+a=hostname 
+echo hello, my saver name is $a
+echo
+echo what is your name?
+read namewho
+echo 
+echo Hello $namewho
+echo
